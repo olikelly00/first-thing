48696f4931e65babd12154329c917c1622821765
Faisal Ahmed <git@fais.al>
2024-09-19 15:28:59 +0200

    its workinggit add .!

--- frontend/first-thing/src/App.jsx
+++ frontend/first-thing/src/App.jsx
@@ -1,40 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
-  //const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
-  const [tasks, setTasks] = useState(['Task 1', 'Task 2', 'Task 3', 'Task 4'])
+	const [tasks, setTasks] = useState([]);
+	const [taskInput, setTaskInput] = useState("");
 
+	function addNewTask(newTask) {
+		setTasks([newTask, ...tasks]);
+	}
 
-  
-  function completeTask(index) {
-    const updatedTasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
-    setTasks(updatedTasks)
-    return updatedTasks
+	function handleTextInput(e) {
+		setTaskInput(e.target.value);
+	}
+
+	function completeTask(index) {
+		const updatedTasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
+		setTasks(updatedTasks);
+		return updatedTasks;
+	}
+
+	return (
+		<>
+			<h1>Welcome to FirstThing</h1>
+			<h3>We're a to-do list application with a twist.</h3>
+			<h3>Add, track and tick off items as you work through them.</h3>
+			<h3>Stuck on where to start? You can prioritise your tasks with our handy AI assistant.</h3>
+			<h2>My list</h2>
+			<ul>
+				{tasks.map((task, index) => (
+					<>
+						<li key={index}>{task}</li> <button onClick={() => completeTask(index)}>Done</button>
+					</>
+				))}
+			</ul>
+			<input value={taskInput} onChange={handleTextInput} type="text"></input>{" "}
+			<button
+				onClick={() => {
+					addNewTask(taskInput);
+					setTaskInput("");
+				}}
+			>
+				Add Task
+			</button>
+		</>
+	);
 }
 
-  return (
-    <>
-      <h1>Welcome to FirstThing</h1>
-      <h3>We're a to-do list application with a twist.</h3>
-      <h3>Add, track and tick off items as you work through them.</h3>
-      <h3>Stuck on where to start? You can prioritise your tasks with our handy AI assistant.</h3>
-
-      <h2>My list</h2>
-      <ul>
-        {tasks.map((task, index) => (
-          <>
-          <li key={index}>{task}</li> <button onClick={() => completeTask(index)}>Done</button>
-          </>
-        ))
-        }
-  
-        </ul>
-    </>
-  )
-}
-
-export default App
+export default App;
