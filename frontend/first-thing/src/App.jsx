80a08119ece239828185593f4f8043285e71a83c
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-09-19 13:58:49 +0100

    fixed frontend bug with done button

--- frontend/first-thing/src/App.jsx
+++ frontend/first-thing/src/App.jsx
@@ -6,16 +6,14 @@ import './App.css'
 function App() {
   
   //const tasks = ['Task 1', 'Task 2', 'Task 3', 'Task 4']
-  const [tasks, setTasks] = useState(['Task 1', 'Task 2', 'Task 3', 'Task 4']);
+  const [tasks, setTasks] = useState(['Task 1', 'Task 2', 'Task 3', 'Task 4'])
 
 
   
   function completeTask(index) {
-    const updatedTasks = tasks.filter((task, i) => i !== index);
+    const updatedTasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)];
     setTasks(updatedTasks)
-    console.log(updatedTasks)
     return updatedTasks
-
 }
 
   return (
@@ -23,15 +21,16 @@ function App() {
       <h1>Welcome to FirstThing</h1>
       <h3>We're a to-do list application with a twist.</h3>
       <h3>Add, track and tick off items as you work through them.</h3>
-      <h3>Stuck on where to start, you can prioritise your tasks with our handy AI assistant.</h3>
+      <h3>Stuck on where to start? You can prioritise your tasks with our handy AI assistant.</h3>
 
       <h2>My list</h2>
       <ul>
         {tasks.map((task, index) => (
           <>
-          <li key={index}>{task}</li> <button onClick={completeTask}>Done</button>
+          <li key={index}>{task}</li> <button onClick={() => completeTask(index)}>Done</button>
           </>
-        ))}
+        ))
+        }
   
         </ul>
     </>
